c9f6c372bf5748273bb793f52d988ce988e8bfa3
Brian Wadlow <bwadlow@gmail.com>
2018-06-25 15:51:53 +0000

    Done.

--- index.js
+++ index.js
@@ -9,10 +9,15 @@ function nestedTarget() {
 function increaseRankBy(n) {
   let list = document.getElementById('app').querySelectorAll('ul.ranked-list li')
   for (let i=0; i<list.length; i++) {
-    list[i].innerHTML = Number((i+1))
+    list[i].innerHTML = Number(list[i].innerHTML)+n
   }
 }
 
 function deepestChild() {
-  
+  var lis=document.getElementById("grand-node").querySelectorAll("div");
+  var test;
+  for(let i=0;i<lis.length-1;i++) {
+    test=lis[i].querySelector("div");
+  }
+return test;
 }
